b7048b3b3d2ab8514e82f6856f323331a482346a
PiFtch <PiFtch@126.com>
2018-04-03 17:44:26 +0800

    Create NAIVE-STRING-MATCHER.cpp

---
+++ NAIVE-STRING-MATCHER.cpp
@@ -0,0 +1,20 @@
+/* description from CLRS p578-579
+NAIVE-STRING-MATCHER(T, P)
+  n = T.length
+  m = P.length
+  for s = 0 to n - m
+    if P[1..m] == T[s+1..s+m]
+      print "Pattern occurs with shifts"
+*/
+
+#include <iostream>
+using namespace std;
+
+NaiveStringMatcher(char[] text, char[] pattern) {
+    
+    
+}
+
+int main() {
+
+}
